17e46bc7dae5b0eee9f27e6d2af579f36aaab601
Domingo <domingo.espinosa@opentrends.net>
2019-09-06 00:27:24 +0200

    Mockito

---
+++ src/test/java/a.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class a {
+    @Test
+    public void test2() {
+
+    }
+}
